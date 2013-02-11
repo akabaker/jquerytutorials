4f954046c0468e3ce1ec71279f8cfe77f31a1d05
Luke Baker <akabaker@gmail.com>
2013-02-10 21:37:14 -0600

    Day 26 - PHP and jQuery Day 1

---
+++ _partials/footer.php
@@ -0,0 +1,3 @@
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
+</body>
+</html>
--- save.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$f = fopen('data.txt', 'w');
-fwrite($f, $_POST['content']);
-fclose($f);
-echo "saved";
